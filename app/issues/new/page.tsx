81914283ba749ab0a907a53b3761bed6ad6d1fdf
cementix <bodakondrackij@gmail.com>
2024-03-08 15:24:00 +0200

    handle form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,21 +1,45 @@
 "use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, TextField } from "@radix-ui/themes";
 import { IoMdCheckmark } from "react-icons/io";
+import { Controller, useForm } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface Issue {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<Issue>();
   return (
-    <div className="max-w-xl space-y-3">
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <TextArea placeholder="Description" />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <Button variant="solid">
         <div className="flex items-center text-lg gap-2">
           <IoMdCheckmark /> New issue
         </div>
       </Button>
-    </div>
+    </form>
   );
 };
 
